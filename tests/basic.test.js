ae15e84ba87b595833e04bd452fa6c3be3bd8b54
Chris Manson <chris@manson.ie>
2026-02-18 17:49:32 +0000

    fix test ordering

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -15,23 +15,22 @@ describe("basic functionality", () => {
       console.log(stdout);
       throw err;
     }
-    expect(parsedOutput).to.deep.equal({
-      name: [
-        "only-its.test.js",
-        "subfolder/thing.test.js",
-        "something.test.js - a basic test",
-        "something.test.js - another basic test",
-        "something.test.js - comes from the outside",
-        "something.test.js - has a friend",
-      ],
-      include: [
-        { name: "only-its.test.js", command: 'vitest only-its.test.js', },
-        { name: "subfolder/thing.test.js", command: 'vitest subfolder/thing.test.js', },
-        { name: "something.test.js - a basic test", command: 'vitest something.test.js --testNamePattern "a basic test"', },
-        { name: "something.test.js - another basic test", command: 'vitest something.test.js --testNamePattern "another basic test"', },
-        { name: "something.test.js - comes from the outside", command: 'vitest something.test.js --testNamePattern "comes from the outside"', },
-        { name: "something.test.js - has a friend", command: 'vitest something.test.js --testNamePattern "has a friend"' },
-      ]
-    })
+    expect(parsedOutput.name).to.have.same.members([
+      "only-its.test.js",
+      "subfolder/thing.test.js",
+      "something.test.js - a basic test",
+      "something.test.js - another basic test",
+      "something.test.js - comes from the outside",
+      "something.test.js - has a friend",
+    ]);
+
+    expect(parsedOutput.include).to.have.same.deep.members([
+      { name: "only-its.test.js", command: 'vitest only-its.test.js', },
+      { name: "subfolder/thing.test.js", command: 'vitest subfolder/thing.test.js', },
+      { name: "something.test.js - a basic test", command: 'vitest something.test.js --testNamePattern "a basic test"', },
+      { name: "something.test.js - another basic test", command: 'vitest something.test.js --testNamePattern "another basic test"', },
+      { name: "something.test.js - comes from the outside", command: 'vitest something.test.js --testNamePattern "comes from the outside"', },
+      { name: "something.test.js - has a friend", command: 'vitest something.test.js --testNamePattern "has a friend"' },
+    ]);
   })
 })
